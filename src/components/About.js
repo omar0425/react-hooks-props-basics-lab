1e7eda39c9cd5b11e44b7442b2e275a70723ae6e
Omar Razi <startfresh87@gmail.com>
2022-02-02 21:49:41 -0500

    commit tree 2c07eb903093df3fc67484e3c0ccdb69eb010cf0

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
-
-function About() {
+import user from "../data/user";
+import Links from "./Links";
+function About(props) {
   return (
-    <div id="about">
+    <div id='about'>
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {props.bio && props.bio !== "" ? <p>{props.bio}</p> : null}
+      {/* <p>{props.bio}</p> */}
+      <img src='https://i.imgur.com/mV8PQxj.gif' alt='I made this' />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
